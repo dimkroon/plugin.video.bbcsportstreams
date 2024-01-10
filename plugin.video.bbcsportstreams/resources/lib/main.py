aaec8b3775270e223a69dc1733ad917463386ae0
Dimitri Kroon <111366411+dimkroon@users.noreply.github.com>
2024-01-10 22:12:50 +0100

    Added some red button dash streams.

--- plugin.video.bbcsportstreams/resources/lib/main.py
+++ plugin.video.bbcsportstreams/resources/lib/main.py
@@ -27,21 +27,31 @@ def root(_):
     for i in range(1, 15):
         stream_name = 'Sport stream {}'.format(i)
         yield Listitem.from_dict(
-            play_stream_live,
+            play_hls_live,
             stream_name,
             params={'channel': stream_name,
                     'url': 'https://ve-hls-push-uk-live.akamaized.net/x=4/i=urn:bbc:pips:service:uk_sport_stream_{:02d}/pc_hd_abr_v2.m3u8'.format(i)})
 
+    nums = ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine ', 'ten', 'eleven', 'twelve')
+    for i in nums:
+        stream_name = 'Red button {}'.format(nums.index(i) + 1)
+        yield Listitem.from_dict(
+            play_dash_live,
+            stream_name,
+            params={'channel': stream_name,
+                    'url': ''.join(('https://vs-cmaf-pushb-uk.live.cf.md.bbci.co.uk/x=4/i=urn:bbc:pips:service:red_button_',
+                                   i, '/pc_hd_abr_v2.mpd'))
+                    })
 
-def create_dash_stream_item(name, manifest_url, resume_time=None):
+def create_stream_item(name, manifest_url, protocol='hls', resume_time=None):
     # noinspection PyImport,PyUnresolvedReferences
     import inputstreamhelper
     logger.debug('dash manifest url: %s', manifest_url)
 
-    PROTOCOL = 'hls'
-    is_helper = inputstreamhelper.Helper(PROTOCOL)
+
+    is_helper = inputstreamhelper.Helper(protocol)
     if not is_helper.check_inputstream():
-        logger.warning('No inputstream handler available for stream type %s', PROTOCOL)
+        logger.warning('No inputstream handler available for stream type %s', protocol)
         return
 
     play_item = Listitem()
@@ -52,7 +62,7 @@ def create_dash_stream_item(name, manifest_url, resume_time=None):
     # play_item.listitem.setMimeType('application/dash+xml')
 
     play_item.property['inputstream'] = is_helper.inputstream_addon
-    play_item.property['inputstream.adaptive.manifest_type'] = PROTOCOL
+    play_item.property['inputstream.adaptive.manifest_type'] = protocol
     # play_item.property['inputstream.adaptive.play_timeshift_buffer'] = 'true'
     # play_item.property['inputstream.adaptive.manifest_update_parameter'] = 'full'
     play_item.property['inputstream.adaptive.stream_headers'] = ''.join((
@@ -67,9 +77,19 @@ def create_dash_stream_item(name, manifest_url, resume_time=None):
 
 
 @Resolver.register
-def play_stream_live(_, channel, url):
+def play_hls_live(_, channel, url):
     logger.info('play live stream - channel=%s, url=%s', channel, url)
-    list_item = create_dash_stream_item(channel, url, resume_time='43200')
+    list_item = create_stream_item(channel, url, resume_time='43200')
     # if list_item:
     #     list_item.property['inputstream.adaptive.manifest_update_parameter'] = 'full'
     return list_item
+
+
+@Resolver.register
+def play_dash_live(_, channel, url):
+    logger.info('play live dash stream - channel=%s, url=%s', channel, url)
+    list_item = create_stream_item(channel, url, protocol='mpd', resume_time='43200')
+    # if list_item:
+    #     list_item.property['inputstream.adaptive.manifest_update_parameter'] = 'full'
+    return list_item
+
